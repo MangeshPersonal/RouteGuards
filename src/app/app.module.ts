bcc6461b0376dc02f80f9f6ea8a9a3e24695de0c
MangeshG <Mangesh.g@intelgain.com>
2018-01-03 18:47:03 +0530

    added Changes for the Activated Route Guards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule} from '@angular/forms'
 
 import { AppComponent } from './app.component';
+import { CanActivateComponent } from './can-activate/can-activate.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CanActivateComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
